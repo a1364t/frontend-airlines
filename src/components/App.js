25fe9cadc532d82d0d1cb419b28dc21b42ae41cd
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-04-27 20:12:37 +0800

    added comments

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import NavBar from "./NavBar";
 
 function App() {
+	// this shouldnt need to be touched again ever
 	return (
 		<div className="App">
 			<BrowserRouter>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+// navlinks here goes to pages route
 function NavBar() {
 	return (
 		<div className="nav-container">
