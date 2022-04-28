6c056deb3a9689cb1a16091984aa4115eb9cd6e2
Alireza Talaei <alireza@Alirezas-MacBook-Pro.local>
2022-04-28 10:21:25 +1000

    flight display is working

---
+++ src/components/FlightDisplay.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+
+
+class FlightDisplay extends Component {
+    constructor() {
+        super();
+        this.state ={
+            flights: {"id" :1, "date" :"3/1/13", "flight_number" :"23", "plane" :"757", "origin" :"JFK", "destination" :"SFO"}
+        }
+    }
+
+    render() {
+        return (
+            <div className='flight_content'>
+                <h2>Flights</h2>                 
+                <h3>Date: {this.state.flights.date}</h3>
+                <h3>Flight: {this.state.flights.flight_number}</h3>
+                <h3>From - TO {this.state.flights.origin} {this.state.flights.destination}</h3>
+                <h3>Plane: {this.state.flights.plane}</h3>              
+            </div>
+        );
+    }
+}
+
+export default FlightDisplay;
